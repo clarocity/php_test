6b423d95506a6b3f99f04392b6c006fa8a6c264e
Maksym Bodnar <maksym.bodnar@gmail.com>
2013-09-02 17:43:53 -0700

    Removed remains of non-working datepicker

--- app/views/header.php
+++ app/views/header.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Bootstrap -->
         <link href="<?php echo BASE_URL . '/css/bootstrap.min.css'; ?>" rel="stylesheet" media="screen">
-        <link href="<?php echo BASE_URL . '/css/datepicker.css'; ?>" rel="stylesheet" media="screen">
 
         <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!--[if lt IE 9]>
@@ -19,7 +18,6 @@
         <script src="//code.jquery.com/jquery.js"></script>
         <!-- Include all compiled plugins (below), or include individual files as needed -->
         <script src="<?php echo BASE_URL . '/js/bootstrap.min.js'; ?>"></script>
-        <script src="<?php echo BASE_URL . '/js/bootstrap-datepicker.js';?>"></script>
         <div class="navbar navbar-inverse">
             <div class="container">
                 <div class="navbar-header">
