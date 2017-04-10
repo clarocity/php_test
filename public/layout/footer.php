64ad58ef0151e6a3d4ef8ac29fdb7ab51c8512fb
Elise <elisecode247@gmail.com>
2017-04-10 02:36:08 -0700

    one last time for fixing links

--- public/layout/footer.php
+++ public/layout/footer.php
@@ -3,12 +3,14 @@
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="  crossorigin="anonymous"></script>
 <script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-<?php if($pageEdit){
+<?php 
+if($pageEdit){
     echo "<script src='javascript/ajaxeditform.js'></script>";
 } else {
-    echo "<script src='javascript/ajaxsearchform.js'></script>";
+    echo "<script src='javascript/ajaxaddform.js'></script>";
 }
 ?>
+<script src='javascript/ajaxsearchform.js'></script>
 
 
 
