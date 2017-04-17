ba6b7f3558feaffc448cd1f4bb97aa7c3cf204bc
Elise <elisecode247@gmail.com>
2017-04-17 09:50:14 -0700

    updates database for dev and prod envs

--- app/db/dbconn.php
+++ app/db/dbconn.php
@@ -1,5 +1,10 @@
 <?php
+if ($_SERVER['SERVER_NAME'] == 'localhost'){
+    require_once "dbprivate.php";
+    $db = new PDO($dsn, $user, $pass);
+} else {
     $dsn = "mysql:host=".getenv('CLEARDB_HOST').";dbname=".getenv('CLEARDB_DBNAME');
     $db = new PDO($dsn, getenv('CLEARDB_USER'), getenv('CLEARDB_PASS'));
+}
 ?>
 
