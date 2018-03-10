c9f16d906fd022bb36846ac3b96563404774cb54
Anna Ly <annatly2@gmail.com>
2018-03-10 07:51:00 -0800

    update database

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO properties_sales (property_id, sale_date, sale_price)
+VALUES ("1", "3/3/2018", "$529,000");
+
+INSERT INTO properties_sales (property_id, sale_date, sale_price)
+VALUES ("2", "3/3/2018", "$875,000");
+
+INSERT INTO properties_sales (property_id, sale_date, sale_price)
+VALUES ("3", "3/4/2018", "$599,999");
+
+INSERT INTO properties (address, city, state, zip)
+VALUES ("774 Winston Dr", "San Diego", "CA", "92114");
+
+INSERT INTO properties (address, city, state, zip)
+VALUES ("7320 Casper Dr", "San Diego", "CA", "92119");
+
+INSERT INTO properties (address, city, state, zip)
+VALUES ("8368 Ivory Coast Dr", "San Diego", "CA", "92126");
